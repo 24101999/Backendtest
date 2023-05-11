28c6e31725b6b1bbafc939089ded6ca768a5104b
Henrique <henriquedasilvacosta@live.com>
2023-05-11 09:27:26 -0300

    ok

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/{id?}', [HomeController::class, 'dados']);
+Route::post('/post', [HomeController::class, 'post']);
+Route::get('/post', [HomeController::class, 'post']);
+Route::delete('/delete/{id?}', [HomeController::class, 'destroy']);
+Route::get('/edit{id?}', [HomeController::class, 'edit']);
+Route::put('/update/{id?}', [HomeController::class, 'update']);
+Route::get('/update/{id?}', [HomeController::class, 'update']);
